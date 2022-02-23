81e28272276ba114a5723fe2b5be7c3b1bfd26a3
rikicop <ruperto1@protonmail.com>
2022-02-22 20:18:31 -0500

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from './components'
+
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing' },
+  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
+]
+
+const Home = () => {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <Head>
+        <title>CMS BLOG</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <div>{<PostCard post={post} key={post.title} />}</div>
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relatve top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
